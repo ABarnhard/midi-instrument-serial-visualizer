1770a4cba406003d886c65a66a9ff49fa796d393
Austin Erlandson <austin@erlandson.com>
2015-03-30 13:11:13 -0500

    Error checking the data, moving on to step 2: Creating an adaptive display of the data. Then step 3 storing the data to S3 as timestamped csv file???

--- js/visualizer.controller.js
+++ js/visualizer.controller.js
@@ -8,113 +8,80 @@ function VisualizerController($scope) {
   var vm     = this,
       serial = chrome.serial,
       connectionInfo,
+      connectionPath = '',
       connectionId;
 
   vm.devices = [];
 
-
-
   function onGetDevices(ports) {
-    var  connectionPath = '';
-
     for (var i = 0; i < ports.length; i++) {
       vm.devices.push(ports[i].path);
     }
-
     connectionPath = vm.connectionPath = vm.devices[5];
     $scope.$apply();
     serial.connect(connectionPath, onConnect);
   }
 
-
-
   function onConnect(info) {
     // The serial port has been opened. Save its id to use later.
     connectionId = vm.id = info.connectionId;
     connectionInfo = vm.connectionInfo = info;
-    // Do whatever you need to do with the opened port.
   }
 
-
   function onRecieveCallback (arrayBuffer) {
-      var obj          = {},
-          u8view       = new Uint8Array(arrayBuffer.data),
-          parsedKVPArr = [],
-          str          = '',
-          sync         = false;
+    var obj          = {},
+        u8view       = new Uint8Array(arrayBuffer.data),
+        parsedKVPArr = [],
+        str          = '',
+        sync         = false;
 
 // Convert ArrayBuffer to a string then an Array.
 
-        for (var a = 0; a < u8view.length; a++) {
-          str += String.fromCharCode(u8view[a]);
-        }
+    for (var a = 0; a < u8view.length; a++) {
+      str += String.fromCharCode(u8view[a]);
+    }
 
-        parsedKVPArr = str.replace(/(\r\n|\n|\r)/gm, ',').replace(/(\s)/g, '').split(',').slice(0, -1);
+    parsedKVPArr = str.replace(/(\r\n|\n|\r)/gm, ',').replace(/(\s)/g, '').split(',').slice(0, -1);
 
 // Convert Parsed Array into an obj with 24 values starting from 0
 
-        for (var c = 0; c < parsedKVPArr.length; c++) {
-          var tempArr = parsedKVPArr[c].split(':');
-          if (sync) {
-            obj[tempArr[0]] = tempArr[1];
-            if (Object.keys(obj).length === 24){
-              console.log(obj);
-              return obj;
-            }
-          } else {
-            if (tempArr[0] === '0') {
-              obj[tempArr[0]] = tempArr[1];
-              sync = true;
-            }
+    for (var c = 0; c < parsedKVPArr.length; c++) {
+      var tempArr = parsedKVPArr[c].split(':');
+      // console.log(tempArr);
+      if (!!tempArr[0] && !!tempArr[1]) {
+        if (sync) {
+          obj[tempArr[0]] = tempArr[1];
+          if (Object.keys(obj).length === 24){
+            // vm.serial = obj;
+            // $scope.$apply();
+            console.log(obj);
           }
+        } else if (tempArr[0] === '0') {
+          obj[tempArr[0]] = tempArr[1];
+          sync = true;
         }
-    // document.querySelector('.serialDisplay').textContent = serialToString(obj.data);
+      } else {
+        console.log('Dropped a Value??? ' + tempArr[0]);
+      }
+    }
   }
 
-
-
   function onReceiveErrorCallback(info) {
-    console.log(info.data);
+    console.log(info);
   }
 
 
 
   document.querySelector('body').onclick = function(){
-    serial.setPaused(connectionId, true, function(){});
+    serial.setPaused(connectionId, true, function() {});
   };
 
-  // console.log(); Current connections logs an array of connection objects.
-  // serial.getConnections(function (arg) {
-  //   console.log(arg);
-  // });
-
-
-  // console.logs a specific connection.
-  // serial.getInfo(connectionId, function (arg) {console.log(arg)});
-
-
-  // Lists available serial devices and appends them to vm.devices.
-
+  // Lists available serial devices and appends them to vm.devices and connects to the controller.
   serial.getDevices(onGetDevices);
-
-
-
-
-  // Connect to the serial port EG: "/dev/tty.usbmodemfa141"
-
 
   // Listen for data from controller.
   serial.onReceive.addListener(onRecieveCallback);
 
-
-  // Listens for errors from data stream. I think???
-  // serial.onReceiveError.addListener(onReceiveErrorCallback);
-
-
-
-
-
-
-
-
+  // Listens for errors from data stream.
+  serial.onReceiveError.addListener(onReceiveErrorCallback);
 }
