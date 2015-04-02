b6f90e9cb6891a1ec4719e31afefc0ee0ffbb283
Austin Erlandson <austin@erlandson.com>
2015-04-01 20:27:03 -0500

    Added a simple bar chart representation. Using d3 with a directive and a controller. Now I just need to hook up the serial controller and create a $watch function in the directive and make sure the dom has time to update with the changes in data from the serial stream and voila!

--- js/visualizer.directive.js
+++ js/visualizer.directive.js
@@ -1,70 +1,21 @@
-angular
-  .module('app')
-  .directive('visualizer', visualizer);
-
-function visualizer() {
-  'use strict';
-
-  var directive = {
-    restrict: 'E',
-    templateUrl: 'js/visualizer.html',
-    controller: 'VisualizerController',
-    controllerAs: 'vis'
-    // link: serialDisplay
-  };
-
-  return directive;
-
-  // function serialDisplay (scope, ele) {
-  //     // our d3 code will go here
-  //   var svg = d3.select(ele[0])
-  //           .append('svg')
-  //           .style('width', '100%');
-
-  //     // Watch for resize event
-  //     scope.$watch(scope.serial, function() {
-  //       scope.render(scope.serial);
-  //     });
-
-  //     scope.render = function(data) {
-
-  //       // If we don't pass any data, return out of the element
-  //       if (!data) return;
-
-  //       // setup variables
-  //       var width = d3.select(ele[0]).node().offsetWidth - 5,
-  //           // calculate the height
-  //           height = Object.keys(scope.data).length * (15 + 5),
-  //           // Use the category20() scale function for multicolor support
-  //           color = d3.scale.category20(),
-  //           // our xScale
-  //           xScale = d3.scale.linear()
-  //             .domain([0, d3.max(data, function(d) {
-  //               return d.score;
-  //             })])
-  //             .range([0, width]);
-
-  //       // set the height based on the calculations above
-  //       svg.attr('height', height);
-
-  //       //create the rectangles for the bar chart
-  //       svg.selectAll('rect')
-  //         .data(data).enter()
-  //           .append('rect')
-  //           .attr('height', 15)
-  //           .attr('width', 140)
-  //           .attr('x', Math.round(5/2))
-  //           .attr('y', function(d,i) {
-  //             return i * (15 + 5);
-  //           })
-  //           .attr('fill', function(d) { return color(d.score); })
-  //           .transition()
-  //             .duration(1000)
-  //             .attr('width', function(d) {
-  //               return xScale(d.score);
-  //             });
-  //     };
-  // }
-}
-
-
+angular.module('app')
+  .directive('bars', function ($parse) {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<div id="chart"></div>',
+      controller: 'MainCtrl',
+      controllerAs: 'vis',
+      link: function (scope, element, attrs) {
+        var data = attrs.data.split(',');
+        d3.select('#chart')
+          .append('div').attr('class', 'chart')
+          .selectAll('div')
+          .data(data).enter()
+          .append('div')
+          .transition().ease('elastic')
+          .style('width', function(d) { return d + 'vw'; })
+          .text(function(d) { return d + '%'; });
+      }
+    };
+  });
